591918a08b867e1d0dba5cda9ef3f36c408f5938
Arthur Jenck <arthurjenckdev@gmail.com>
2025-10-18 01:54:50 +0200

    started contact

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,26 +1,17 @@
 import React from "react"
 import "../styles/Contact.scss"
 import CopyBtn from "../components/CopyBtn"
+import SectionTitle from "@/components/SectionTitle"
 
-const Contact = ({
-    spiralTurn = (e: React.MouseEvent) => {
-        e
-    },
-}) => {
+const Contact = () => {
     const mail = "contact@arthurjenck.com"
     const tel = "0610790509"
     const linkedinLink = "https://www.linkedin.com/in/arthurjenck/"
     const githubLink = "https://github.com/ArthurJenck/"
 
     return (
-        // La spirale de la section À propos tourne aussi quand la souris se déplace dans la section Contact
-        <section
-            id="contact"
-            onMouseMove={(e) => {
-                spiralTurn(e)
-            }}
-        >
-            <h2>On prend un café ?</h2>
+        <section id="contact">
+            <SectionTitle title="On prend un café ?" />
             <ul className="about-links">
                 <li>
                     {/* Le lien doit directement permettre d'envoyer un mail */}
