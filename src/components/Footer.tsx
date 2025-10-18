77ac31144b0c83cf25f0d755727d57fcfaa2ee31
Arthur Jenck <arthurjenckdev@gmail.com>
2025-10-18 14:03:22 +0200

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,16 @@
 import ImgLink from "./ImgLink"
-import "../styles/Footer.scss"
 
 const Footer = () => {
     return (
-        <footer>
-            <ImgLink type="logo" />
-            <p>
-                Merci d'être passé, ça vous a plu ?
-                <span>N'hésitez pas à me le dire !</span>
-            </p>
-            <div className="socials">
+        <footer className="relative bg-[var(--secondary)] flex flex-col md:flex-row justify-between items-center py-4 md:py-[2vw] px-0 md:px-[4vw] gap-4 md:gap-0">
+            <div className="flex flex-col md:flex-row items-center gap-[1.5vw]">
+                <ImgLink type="logo" className="size-16" />
+                <p className="font-bold tracking-[1px] text-center">
+                    Merci d'être passé, ça vous a plu ?
+                    <span className="block">N'hésitez pas à me le dire !</span>
+                </p>
+            </div>
+            <div className="socials flex justify-center items-center gap-2">
                 <ImgLink type="linkedin" />
                 <ImgLink type="github" />
             </div>
