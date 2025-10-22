dc400c0961330d65251eb65219f6fe390c559cf6
Arthur Jenck <arthurjenckdev@gmail.com>
2025-10-22 22:45:21 +0200

    wip

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import { Sora, Montserrat } from 'next/font/google'
+import { Sora } from 'next/font/google'
 import '@/app/globals.css'
 import { QueryProvider } from '@/providers/QueryProvider'
 import { PrefetchProvider } from '@/providers/PrefetchProvider'
@@ -10,12 +10,6 @@ import NavBar from '@/components/NavBar'
 import Footer from '@/components/Footer'
 
 const sora = Sora({
-    subsets: ['latin'],
-    weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
-    display: 'swap',
-})
-
-const montserrat = Montserrat({
     subsets: ['latin'],
     weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
     display: 'swap',
@@ -74,7 +68,7 @@ export default function RootLayout({
                     }}
                 />
             </head>
-            <body className={cn('overflow-x-hidden', sora.className, montserrat.className)}>
+            <body className={cn('overflow-x-hidden', sora.className)}>
                 <QueryProvider>
                     <PrefetchProvider>
                         <div className="min-h-screen flex flex-col">
--- src/components/BurgerLink.tsx
+++ src/components/BurgerLink.tsx
@@ -1,31 +1,96 @@
 import { useScroll } from '@/hooks/useScroll'
 import { usePathname } from 'next/navigation'
+import Link from 'next/link'
+import { useState, useEffect } from 'react'
+import localFont from 'next/font/local'
+import { Ballet, Festive, Limelight, Meddon } from 'next/font/google'
 
 interface BurgerLinkProps {
     href?: string
     title: string
 }
 
+const mtfDoodle = localFont({
+    src: '../assets/fonts/MTFDoodle.otf',
+})
+
+const limelight = Limelight({
+    subsets: ['latin'],
+    weight: ['400'],
+    display: 'swap',
+})
+
+const ballet = Ballet({
+    subsets: ['latin'],
+    weight: ['400'],
+    display: 'swap',
+})
+
+const meddon = Meddon({
+    subsets: ['latin'],
+    weight: ['400'],
+    display: 'swap',
+})
+
+const festive = Festive({
+    subsets: ['latin'],
+    weight: ['400'],
+    display: 'swap',
+})
+
+const fonts = [
+    'Sora',
+    limelight.style.fontFamily,
+    ballet.style.fontFamily,
+    meddon.style.fontFamily,
+    festive.style.fontFamily,
+    mtfDoodle.style.fontFamily,
+]
+
 const BurgerLink = ({ href, title }: BurgerLinkProps) => {
     const scrollTo = useScroll()
     const pathname = usePathname()
     const isHomePage = pathname === '/'
+    const [isHovered, setIsHovered] = useState(false)
+    const [currentFontIndex, setCurrentFontIndex] = useState(0)
+
+    useEffect(() => {
+        if (isHovered) {
+            const interval = setInterval(() => {
+                setCurrentFontIndex((prevIndex) => (prevIndex + 1) % fonts.length)
+            }, 350)
+
+            return () => clearInterval(interval)
+        }
+    }, [isHovered])
+
+    const handleMouseEnter = () => {
+        setIsHovered(true)
+    }
+
+    const handleMouseLeave = () => {
+        setIsHovered(false)
+        setCurrentFontIndex(0)
+    }
 
     return (
         <li>
             {/* Le preventDefault sert à éviter le rechargement de la page en cliquant sur le lien. On utilise alors la fonction scrollTo pour remonter en haut de la page et nettoyer l'url */}
-            <a
+            <Link
                 className="burger-link font-bold text-2xl tracking-[1px] relative select-none cursor-pointer"
-                href={isHomePage ? href : `/${href}`}
+                href={isHomePage ? href || '/' : `/${href || ''}`}
                 onClick={(e) => {
                     if (!href && isHomePage) {
                         e.preventDefault()
                         scrollTo(0)
                     }
                 }}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+                style={{ fontFamily: fonts[currentFontIndex] }}
             >
                 {title}
-            </a>
+            </Link>
         </li>
     )
 }
