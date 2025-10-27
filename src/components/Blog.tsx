20ade3f9280830ba5192e5e6a3a34496a1369689
Arthur Jenck <arthurjenckdev@gmail.com>
2025-10-27 21:49:13 +0100

    feat: created blog file

---
+++ src/components/Blog.tsx
@@ -0,0 +1,10 @@
+const Blog = () => {
+    return (
+        <div>
+            <script src="https://elfsightcdn.com/platform.js" async></script>
+            <div className="elfsight-app-9acdf36e-7b58-4fa4-bf75-181b3b285aae" data-elfsight-app-lazy></div>
+        </div>
+    )
+}
+
+export default Blog
