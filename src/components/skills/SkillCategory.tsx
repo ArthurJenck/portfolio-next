2823eb2f4a654a848fa16f970981e437dac0e735
Arthur <arthurjenckdev@gmail.com>
2026-07-21 16:33:33 +0200

    feat/skills: added gradient line

--- src/components/skills/SkillCategory.tsx
+++ src/components/skills/SkillCategory.tsx
@@ -10,10 +10,10 @@ interface SkillCategoryProps {
 const SkillCategory = ({ name, truncatedName, skills }: SkillCategoryProps) => {
     return (
         <div className="w-fit relative left-[5.5vw] lg:left-[15vw] top-0 mt-[7svh] lg:mt-[13svh]">
-            <h3 className="md:hidden font-bold sticky top-[50svh] text-[clamp(1.2rem,2.5vw,2.5vw)] float-left text-right tracking-[2px] min-w-16">
+            <h3 className="md:hidden font-bold sticky top-[50svh] text-[clamp(1.2rem,2.5vw,2.5vw)] float-left text-right tracking-[2px] min-w-16 bg-[var(--primary)] z-10">
                 {truncatedName}
             </h3>
-            <h3 className="hidden md:block font-bold sticky top-[50svh] text-[2.5vw] float-left text-right tracking-[2px] min-w-[clamp(120px,14vw,14vw)]">
+            <h3 className="hidden md:block font-bold sticky top-[50svh] text-[2.5vw] float-left text-right tracking-[2px] min-w-[clamp(120px,14vw,14vw)] bg-[var(--primary)] z-10">
                 {name}
             </h3>
             <ul className="list-none flex flex-col gap-6 lg:gap-10 top-0 max-w-[68vw] lg:max-w-[55vw] pl-[3vw]">
